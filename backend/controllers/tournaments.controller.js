9942708501aa5b4c9fb65e05482375dd6b8785a1
Sacha Macé <sacha.mace@gmail.com>
2026-02-18 11:20:42 +0100

    :jigsaw: start controller in tournaments

--- backend/controllers/tournaments.controller.js
+++ backend/controllers/tournaments.controller.js
@@ -1,2 +1,75 @@
+
 const TournamentsModel = require('../models/tournaments.model');
+
+
+// Les controllers GET
+
+module.exports.getTournaments = async(req,res) =>{
+    const tournaments = await TournamentsModel.find();
+    res.status(200).json(tournaments);
+};
+
+module.exports.getTournament = async(req,res) =>{
+    const tournament = await TournamentsModel.findById(req.params.id);
+    res.status(200).json(tournament);
+};
+
+module.exports.getBracket = async(req,res) => {
     
+};
+
+module.exports.getTeamsRegister = async(req,res) => {
+
+};
+
+module.exports.getRanking = async(req,res) => {
+
+};
+
+// Les controllers POST 
+
+module.exports.addTournament = async(req,res) => {
+
+};
+
+module.exports.addTeamsAtTournament = async(req,res) => {
+
+};
+
+module.exports.startTournament = async(req,res) => {
+
+};
+
+module.exports.cancelTournament = async (req,res) => {
+    
+};
+
+// Les controllers Patch
+
+module.exports.changeStatutTournament = async(req,res) =>{
+
+};
+
+// Les controllers Delete 
+module.exports.deleteTournament = async(req,res) =>{
+
+};
+
+module.exports.unsubscribeTeam = async(req,res) =>{
+
+};
+
+module.exports.deleteAllMatchsTournament = async(req,res)=>{
+
+};
+
+// Les controlers PUT
+
+module.exports.replaceAll = async(req,res) =>{
+
+};
+
+module.exports.validateListFinaleTournament = async(req,res) =>{
+
+};
+
